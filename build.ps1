e16162e85750f6f9cd8779aae39b352c0220b8ee
Serge Weinstock <serge.weinstock@gmail.com>
2024-11-06 13:22:00 +0000

    still working on the installation/build

--- build.ps1
+++ build.ps1
@@ -1,15 +1,45 @@
+<#
+.SYNOPSIS
+  Launch a full build/install for one or more build types
+.PARAMETER BuildTypes
+  Which build type to generate: Debug, Release, RelWithDebInfo or a combination?
+.PARAMETER Clean
+  Remove build folder first?
+#>
+
+param(  
+    [ValidateNotNull()]
+    [ValidateSet('', 'Debug', 'Release', 'RelWithDebInfo', 'MinSizeRel')]
+    [string[]] $BuildTypes,
+
+    [ValidateNotNullOrEmpty()]
+    [switch] $Clean
+)
+
+Write-Host "Build Types: $BuildTypes"
+
+$src_root=${PSScriptRoot}
+$os = $IsWindows ? "windows" : ($IsLinux ? "Linux" : "osx")
+
 $common_options = @(
-    "-DCMAKE_C_COMPILER=cl",
-    "-DCMAKE_CXX_COMPILER=cl",
-    "-DCMAKE_INSTALL_PREFIX=D:/src/Grafology/dist/Windows",
-    "-DCMAKE_TOOLCHAIN_FILE=D:/src/vcpkg/scripts/buildsystems/vcpkg.cmake",
+    "-DCMAKE_INSTALL_PREFIX=${src_root}/dist/${os}",
     "-DVCPKG_APPLOCAL_DEPS=ON",
     "-DX_VCPKG_APPLOCAL_DEPS_INSTALL=ON",
-    "-DVCPKG_TARGET_TRIPLET=x64-windows",
+    "-DVCPKG_TARGET_TRIPLET=x64-${os}",
     "-DVCPKG_MANIFEST_MODE=ON",
-    "-SD:/src/Grafology",
+    "-S${src_root}",
     "-G Ninja"
 )
 
-$build_type = "Release"
-& cmake "-DCMAKE_BUILD_TYPE=${build_type}" "-BD:/src/Grafology/bin/Windows/${build_type}" $common_options
+$BuildTypes | ForEach-Object {
+    $build_type = $_
+    if ($Clean) {
+        Write-Host "============> Cleaning $build_type"
+        Remove-Item -Force -Recurse "${src_root}/bin/${os}/${build_type}"
+    }
+    $preset="win-x64-${build_type}"
+    Write-Host "============> Building $preset"
+    & cmake --preset ${preset} $common_options
+    & cmake --build "${src_root}/bin/${os}/${build_type}"
+    & cmake --install "${src_root}/bin/${os}/${build_type}"
+}
