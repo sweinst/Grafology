3de25f9eb08c245631c6dfc89cd77e0a3d9b04b2
Serge Weinstock <serge.weinstock@gmail.com>
2024-12-07 08:10:40 +0000

    Added clang-format options

--- examples/london_tube.h
+++ examples/london_tube.h
@@ -1,104 +1,103 @@
 #pragma once
 #include <grafology/grafology.h>
 #include <map>
-#include <set>
 #include <optional>
+#include <set>
 
 // a graph of London Tube stations and connections
-// derived from data found at https://www.whatdotheyknow.com/request/station_to_station_journey_times
+// derived from data found at
+// https://www.whatdotheyknow.com/request/station_to_station_journey_times
 
 namespace LondonTube {
-    namespace g = grafology;
+namespace g = grafology;
 
-    struct Line {
-        unsigned _id;
-        std::string _name {};
+struct Line {
+  unsigned _id;
+  std::string _name{};
 
-        auto operator <=>(const Line& l) const noexcept {
-            return _id <=> l._id;
-        }
-        bool operator ==(const Line& l) const noexcept {
-            return _id == l._id;
-        }
+  auto operator<=>(const Line& l) const noexcept { return _id <=> l._id; }
+  bool operator==(const Line& l) const noexcept { return _id == l._id; }
 
-        static std::optional<Line> from_id(unsigned id);
-    };
-    extern std::set<Line> lines;
+  static std::optional<Line> from_id(unsigned id);
+};
+extern std::set<Line> lines;
 
-    struct Station{
-        g::vertex_t _id;
-        std::string _name {};
-        double _latitude = 0;
-        double _longitude = 0;
+struct Station {
+  g::vertex_t _id;
+  std::string _name{};
+  double _latitude = 0;
+  double _longitude = 0;
 
-        auto operator <=>(const Station& s) const noexcept {
-            return _id <=> s._id;
-        }
-        bool operator ==(const Station& s) const noexcept {
-            return _id == s._id;
-        }
+  auto operator<=>(const Station& s) const noexcept { return _id <=> s._id; }
+  bool operator==(const Station& s) const noexcept { return _id == s._id; }
 
-        static std::optional<Station> from_id(g::vertex_t id);
-        static std::optional<Station> from_name(const std::string& name);
+  static std::optional<Station> from_id(g::vertex_t id);
+  static std::optional<Station> from_name(const std::string& name);
 
-        double distance_from(const Station& s) const;
-    };
+  double distance_from(const Station& s) const;
+};
 
-    extern std::set<Station> stations;
-} // namespace LondonTube
-
-namespace std {
-    namespace lt = LondonTube;
-
-    template <>
-    struct hash<lt::Station> {
-        std::size_t operator() (const lt::Station& v) const {
-            return std::hash<int>{}(v._id);
-        }
-    };
-
-    template <>
-    struct formatter<lt::Station, char> {
-        template <class ParseContext>
-        constexpr ParseContext::iterator parse(ParseContext& ctx) {
-            return ctx.begin();
-        }
-
-        template <class FormatContext>
-        auto format(const lt::Station& v, FormatContext& ctx) const {
-            return std::format_to(ctx.out(), "[{}: {}]", v._name, v._id);
-        }
-    };
-}
-
-namespace LondonTube {
-    struct Connection: public g::EdgeDefinition<Station> {
-        // the weight os the distance in kms
-        unsigned line = 0;
-        double time_unimpeded_sec;
-        double time_peak_am_sec;
-        double time_inter_peak_sec;
-
-
-        Connection(g::vertex_t start, g::vertex_t end, double distance_km = 0, unsigned line = 0, double u = 0, double p = 0, double i = 0):
-            g::EdgeDefinition<Station>{{start}, {end}, g::weight_t(distance_km * 1'000)},
-            line{line}, time_unimpeded_sec(u), time_peak_am_sec(p), time_inter_peak_sec(i) {}
-
-
-        std::strong_ordering operator <=>(const Connection& c) const noexcept {
-            auto cmp = start <=> c.start;
-            if (cmp != std::strong_ordering::equal)  {
-                return cmp;
-            }
-            return end <=> c.end;
-        }
-
-        static std::optional<Connection> from_id(g::vertex_t start, g::vertex_t end);
-    };
-    
-    extern std::set<Connection> connections;
-
+extern std::set<Station> stations;
 }  // namespace LondonTube
 
 namespace std {
-}
+namespace lt = LondonTube;
+
+template <>
+struct hash<lt::Station> {
+  std::size_t operator()(const lt::Station& v) const {
+    return std::hash<int>{}(v._id);
+  }
+};
+
+template <>
+struct formatter<lt::Station, char> {
+  template <class ParseContext>
+  constexpr ParseContext::iterator parse(ParseContext& ctx) {
+    return ctx.begin();
+  }
+
+  template <class FormatContext>
+  auto format(const lt::Station& v, FormatContext& ctx) const {
+    return std::format_to(ctx.out(), "[{}: {}]", v._name, v._id);
+  }
+};
+}  // namespace std
+
+namespace LondonTube {
+struct Connection : public g::EdgeDefinition<Station> {
+  // the weight os the distance in kms
+  unsigned line = 0;
+  double time_unimpeded_sec;
+  double time_peak_am_sec;
+  double time_inter_peak_sec;
+
+  Connection(g::vertex_t start,
+             g::vertex_t end,
+             double distance_km = 0,
+             unsigned line = 0,
+             double u = 0,
+             double p = 0,
+             double i = 0)
+      : g::EdgeDefinition<Station>{{start},
+                                   {end},
+                                   g::weight_t(distance_km * 1'000)},
+        line{line},
+        time_unimpeded_sec(u),
+        time_peak_am_sec(p),
+        time_inter_peak_sec(i) {}
+
+  std::strong_ordering operator<=>(const Connection& c) const noexcept {
+    auto cmp = start <=> c.start;
+    if (cmp != std::strong_ordering::equal) {
+      return cmp;
+    }
+    return end <=> c.end;
+  }
+
+  static std::optional<Connection> from_id(g::vertex_t start, g::vertex_t end);
+};
+
+extern std::set<Connection> connections;
+
+}  // namespace LondonTube
--- src/include/grafology/algorithms/all_shortest_paths.h
+++ src/include/grafology/algorithms/all_shortest_paths.h
@@ -1,141 +1,145 @@
 #pragma once
-#include "../graph.h"
 #include <queue>
+#include "../graph.h"
 
 namespace grafology {
-
-    /**
-     * @brief This struct allows to process the results of the algorithm shortest_paths
-     */
-    struct AllShortestPathsImpl {
-        AllShortestPathsImpl(size_t n_vertices, vertex_t start)
-        : _distances(n_vertices, D_INFINITY), 
+  /**
+   * @brief This struct allows to process the results of the algorithm
+   * shortest_paths
+   */
+  struct AllShortestPathsImpl {
+    AllShortestPathsImpl(size_t n_vertices, vertex_t start)
+        : _distances(n_vertices, D_INFINITY),
           _predecessors(n_vertices, NO_PREDECESSOR),
           _start(start) {}
-        AllShortestPathsImpl(AllShortestPathsImpl&&) = default;
-        AllShortestPathsImpl(const AllShortestPathsImpl&) = default;
-        AllShortestPathsImpl& operator=(AllShortestPathsImpl&&) = default;
-        AllShortestPathsImpl& operator=(const AllShortestPathsImpl&) = default;
+    AllShortestPathsImpl(AllShortestPathsImpl&&) = default;
+    AllShortestPathsImpl(const AllShortestPathsImpl&) = default;
+    AllShortestPathsImpl& operator=(AllShortestPathsImpl&&) = default;
+    AllShortestPathsImpl& operator=(const AllShortestPathsImpl&) = default;
 
-        std::vector<weight_t> _distances;
-        std::vector<vertex_t> _predecessors;
-        const vertex_t _start;
+    std::vector<weight_t> _distances;
+    std::vector<vertex_t> _predecessors;
+    const vertex_t _start;
 
-        auto size() const {
-            return _distances.size();
-        }
+    auto size() const { return _distances.size(); }
 
-        bool is_reachable(vertex_t end) const {
-            return _distances[end] != D_INFINITY;
-        }
-
-        std::vector<vertex_t> get_path(vertex_t end) const {
-            std::vector<vertex_t> path;
-            if (_distances[end] == D_INFINITY) {
-                return path;
-            }
-            auto current = end;
-            while (current != _start) {
-                path.push_back(current);
-                current = _predecessors[current];
-            }
-            path.push_back(_start);
-            std::reverse(path.begin(), path.end());
-            return path;
-        }
-    };
-
-    /**
-     * @brief Compute the shortest paths from a vertex to all other vertices in a graph
-     * @remark this is based on the Dijkstra's algorithm
-     */
-    template <GraphImpl G>
-    AllShortestPathsImpl all_shortest_paths(const G& graph, vertex_t start) {
-        if (graph.is_directed()) {
-            throw error("Shortest paths works only on undirected graphs");
-        }
-
-        const auto n_vertices = graph.size();
-        AllShortestPathsImpl res(n_vertices, start);
-        std::priority_queue<std::pair<weight_t, vertex_t>> pq;
-
-        res._distances[start] = 0;
-        pq.push({0, start});
-        while (!pq.empty()) {
-            if (pq.empty()) {
-                break;
-            }
-            auto [d, v] = pq.top();
-            pq.pop();
-            if (d > res._distances[v]) {
-                continue;
-            }
-            for (const auto& edge : graph.get_neighbors(v)) {
-                if (edge.weight <= 0) {
-                    throw error("All shortest paths: negative weights are not allowed");
-                }
-                auto new_d = d + edge.weight;
-                if (new_d < res._distances[edge.end]) {
-                    res._distances[edge.end] = new_d;
-                    res._predecessors[edge.end] = v;
-                    pq.push({new_d, edge.end});
-                }
-            }
-        }
-        return res;
+    bool is_reachable(vertex_t end) const {
+      return _distances[end] != D_INFINITY;
     }
 
-    /**
-     * @brief Compute the shortest paths from a vertex to all other vertices in a graph
-     * @remark this is based on the Dijkstra's algorithm
-     */
-    template<GraphImpl Impl, VertexKey Vertex, bool IsDirected>
-    struct AllShortestPaths {
-        AllShortestPaths(AllShortestPathsImpl&& shortest_paths, const Graph<Impl, Vertex, IsDirected>& graph)
-        : _shortest_paths(std::move(shortest_paths)), graph(graph)
-        {}
-        AllShortestPaths(const AllShortestPaths&) = default;
-        AllShortestPaths(AllShortestPaths&&) = default;
-        AllShortestPaths& operator=(const AllShortestPaths&) = default;
-        AllShortestPaths& operator=(AllShortestPaths&&) = default;
+    std::vector<vertex_t> get_path(vertex_t end) const {
+      std::vector<vertex_t> path;
+      if (_distances[end] == D_INFINITY) {
+        return path;
+      }
+      auto current = end;
+      while (current != _start) {
+        path.push_back(current);
+        current = _predecessors[current];
+      }
+      path.push_back(_start);
+      std::reverse(path.begin(), path.end());
+      return path;
+    }
+  };
 
-        auto size() const {
-            return _shortest_paths._distances.size();
-        }
-
-        bool is_reachable(const Vertex& v) const {
-            return _shortest_paths._distances[graph.get_internal_index(v)] != D_INFINITY;
-        }
-
-        weight_t get_distance(const Vertex& v) const {
-            return _shortest_paths._distances[graph.get_internal_index(v)];
-        }
-
-        const Vertex& get_predecessor(const Vertex& v) const {
-            return graph.get_vertex_from_internal_index(_shortest_paths._predecessors[graph.get_internal_index(v)]);
-        }
-
-        generator<Vertex> get_path(const Vertex& v) const {
-            auto sp = _shortest_paths.get_path(graph.get_internal_index(v));
-            for (auto v : sp) {
-                co_yield graph.get_vertex_from_internal_index(v);
-            }
-        }
-
-    private:
-        const AllShortestPathsImpl _shortest_paths;
-        const Graph<Impl, Vertex, IsDirected>& graph;
-    };
-
-    template<GraphImpl Impl, VertexKey Vertex>
-    AllShortestPaths<Impl, Vertex, false> all_shortest_paths(const Graph<Impl, Vertex, false>& graph, const Vertex& start) {
-        auto sp_impl = all_shortest_paths(graph.impl(), graph.get_internal_index(start));
-        return AllShortestPaths<Impl, Vertex, false>(std::move(sp_impl), graph);
+  /**
+   * @brief Compute the shortest paths from a vertex to all other vertices in a
+   * graph
+   * @remark this is based on the Dijkstra's algorithm
+   */
+  template <GraphImpl G>
+  AllShortestPathsImpl all_shortest_paths(const G& graph, vertex_t start) {
+    if (graph.is_directed()) {
+      throw error("Shortest paths works only on undirected graphs");
     }
 
-    template<GraphImpl Impl, VertexKey Vertex>
-    AllShortestPaths<Impl, Vertex, true> all_shortest_paths(const Graph<Impl, Vertex, true>& graph, const Vertex& start) {
-        static_assert(false, "Shortest paths works only on undirected graphs");
-    }
-}
+    const auto n_vertices = graph.size();
+    AllShortestPathsImpl res(n_vertices, start);
+    std::priority_queue<std::pair<weight_t, vertex_t>> pq;
 
+    res._distances[start] = 0;
+    pq.push({0, start});
+    while (!pq.empty()) {
+      if (pq.empty()) {
+        break;
+      }
+      auto [d, v] = pq.top();
+      pq.pop();
+      if (d > res._distances[v]) {
+        continue;
+      }
+      for (const auto& edge : graph.get_neighbors(v)) {
+        if (edge.weight <= 0) {
+          throw error("All shortest paths: negative weights are not allowed");
+        }
+        auto new_d = d + edge.weight;
+        if (new_d < res._distances[edge.end]) {
+          res._distances[edge.end] = new_d;
+          res._predecessors[edge.end] = v;
+          pq.push({new_d, edge.end});
+        }
+      }
+    }
+    return res;
+  }
+
+  /**
+   * @brief Compute the shortest paths from a vertex to all other vertices in a
+   * graph
+   * @remark this is based on the Dijkstra's algorithm
+   */
+  template <GraphImpl Impl, VertexKey Vertex, bool IsDirected>
+  struct AllShortestPaths {
+    AllShortestPaths(AllShortestPathsImpl&& shortest_paths,
+                     const Graph<Impl, Vertex, IsDirected>& graph)
+        : _shortest_paths(std::move(shortest_paths)), graph(graph) {}
+    AllShortestPaths(const AllShortestPaths&) = default;
+    AllShortestPaths(AllShortestPaths&&) = default;
+    AllShortestPaths& operator=(const AllShortestPaths&) = default;
+    AllShortestPaths& operator=(AllShortestPaths&&) = default;
+
+    auto size() const { return _shortest_paths._distances.size(); }
+
+    bool is_reachable(const Vertex& v) const {
+      return _shortest_paths._distances[graph.get_internal_index(v)] !=
+             D_INFINITY;
+    }
+
+    weight_t get_distance(const Vertex& v) const {
+      return _shortest_paths._distances[graph.get_internal_index(v)];
+    }
+
+    const Vertex& get_predecessor(const Vertex& v) const {
+      return graph.get_vertex_from_internal_index(
+          _shortest_paths._predecessors[graph.get_internal_index(v)]);
+    }
+
+    generator<Vertex> get_path(const Vertex& v) const {
+      auto sp = _shortest_paths.get_path(graph.get_internal_index(v));
+      for (auto v : sp) {
+        co_yield graph.get_vertex_from_internal_index(v);
+      }
+    }
+
+   private:
+    const AllShortestPathsImpl _shortest_paths;
+    const Graph<Impl, Vertex, IsDirected>& graph;
+  };
+
+  template <GraphImpl Impl, VertexKey Vertex>
+  AllShortestPaths<Impl, Vertex, false> all_shortest_paths(
+      const Graph<Impl, Vertex, false>& graph,
+      const Vertex& start) {
+    auto sp_impl =
+        all_shortest_paths(graph.impl(), graph.get_internal_index(start));
+    return AllShortestPaths<Impl, Vertex, false>(std::move(sp_impl), graph);
+  }
+
+  template <GraphImpl Impl, VertexKey Vertex>
+  AllShortestPaths<Impl, Vertex, true> all_shortest_paths(
+      const Graph<Impl, Vertex, true>& graph,
+      const Vertex& start) {
+    static_assert(false, "Shortest paths works only on undirected graphs");
+  }
+}  // namespace grafology
