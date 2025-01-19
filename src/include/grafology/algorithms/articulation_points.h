d6c96ce124f01a07fbe525a72d337d8ecd82409a
Serge Weinstock <serge.weinstock@gmail.com>
2025-01-19 10:34:32 +0000

    A little code cleaning

--- src/include/grafology/algorithms/articulation_points.h
+++ src/include/grafology/algorithms/articulation_points.h
@@ -15,8 +15,7 @@ namespace grafology {
             throw error("Articulation_points works only on undirected graphs");
         }
         auto V = graph.size();
-        constexpr int UNDEFINED = std::numeric_limits<int>::max();
-        std::vector<bool> visited(V, false);
+        constexpr int UNDEFINED = -1;
         std::vector<bool> is_articulation(V, false);
         std::vector<int> discovery_time(V, UNDEFINED);
         std::vector<int> lowest_time(V, UNDEFINED);
@@ -25,7 +24,7 @@ namespace grafology {
         int timer = 0;
 
         for (int u = 0; u < V; ++u) {
-            if (!visited[u]) {
+            if (discovery_time[u] == UNDEFINED) {
                 // This is a non-recursive version of Tarjan's algorithm
                 // To achieve it, one needs to process a node in 3 phases:
                 // - first visit the node (initialize the lowest time and the discovery time)
@@ -45,10 +44,9 @@ namespace grafology {
                     if (pre_process) {
                         auto p = parent[v];
                         discovery_time[v] = lowest_time[v] = time;
-                        visited[v] = true;
                         ++time;
                         for (auto w : graph.get_raw_neighbors(v)) {
-                            if (!visited[w]) {
+                            if (discovery_time[w] == UNDEFINED) {
                                 parent[w] = v;
                                 stack.push({w, false});
                                 stack.push({w, true});
--- src/include/grafology/algorithms/bridges.h
+++ src/include/grafology/algorithms/bridges.h
@@ -14,15 +14,15 @@ namespace grafology {
         if (graph.is_directed()) {
             throw error("Bridges works only on undirected graphs");
         }
+        constexpr int UNDEFINED = -1;
         auto V = graph.size();
-        std::vector<bool> visited(V, false);
-        std::vector<int> discovery_time(V, std::numeric_limits<int>::max());
-        std::vector<int> lowest_time(V, std::numeric_limits<int>::max());
+        std::vector<int> discovery_time(V, UNDEFINED);
+        std::vector<int> lowest_time(V, UNDEFINED);
         std::vector<vertex_t> parent(V, NO_PREDECESSOR);
         int timer = 0;
 
         for (int u = 0; u < V; ++u) {
-            if (!visited[u]) {
+            if (discovery_time[u] == UNDEFINED) {
                 // This is a non-recursive version of Tarjan's algorithm
                 // To achieve it, one needs to process a node in 3 phases:
                 // - first visit the node (initialize the lowest time and the discovery time)
@@ -42,10 +42,9 @@ namespace grafology {
                     if (pre_process) {
                         auto p = parent[v];
                         discovery_time[v] = lowest_time[v] = time;
-                        visited[v] = true;
                         time++;
                         for (auto w : graph.get_raw_neighbors(v)) {
-                            if (!visited[w]) {
+                            if (discovery_time[w] == UNDEFINED) {
                                 parent[w] = v;
                                 stack.push({w, false});
                                 stack.push({w, true});
