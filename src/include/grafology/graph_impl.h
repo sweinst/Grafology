8a282678c53ac8ee0c04b932d000877bb6fe96c4
Serge Weinstock <serge.weinstock@gmail.com>
2025-03-08 13:09:13 +0000

    Starting refactoring for moving base types into a traits class

--- src/include/grafology/graph_impl.h
+++ src/include/grafology/graph_impl.h
@@ -1,26 +1,10 @@
 #pragma once
 #include <cassert>
 #include <concepts>
-#include <format>
 #include <ranges>
-#include <stdexcept>
-#include <string>
-#include "internal/generator.h"
+#include "utilities.h"
 
 namespace grafology {
-
-  /** @brief a grafology exception
-   * @details This exception allows the definition of errors specific to this library.
-   */
-  class error : public std::runtime_error {
-   public:
-    // inherit the constructors
-    using std::runtime_error::runtime_error;
-    // allow to use std::format directly in the constructor
-    template <class... Args>
-    error(std::format_string<Args...> fmt, Args&&... args)
-        : std::runtime_error(std::format(fmt, std::forward<Args>(args)...)) {}
-  };
 
   //** the internal vertex type
   using vertex_t = unsigned;
---
+++ src/include/grafology/utilities.h
@@ -0,0 +1,20 @@
+#include <format>
+#include <stdexcept>
+#include <string>
+#include "internal/generator.h"
+
+namespace grafology {
+    /** @brief a grafology exception
+     * @details This exception allows the definition of errors specific to this library.
+     */
+    class error : public std::runtime_error {
+       public:
+        // inherit the constructors
+        using std::runtime_error::runtime_error;
+        // allow to use std::format directly in the constructor
+        template <class... Args>
+        error(std::format_string<Args...> fmt, Args&&... args)
+            : std::runtime_error(std::format(fmt, std::forward<Args>(args)...)) {}
+    };
+
+}  // namespace grafology
